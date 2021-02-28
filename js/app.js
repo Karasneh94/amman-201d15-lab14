780dcbdf27c0d695424417bc8e3ef83282ae0021
Ahmed Karasneh <karasneh94@gmail.com>
2021-02-28 15:00:38 +0200

    first actual commit

--- js/app.js
+++ js/app.js
@@ -1,40 +1,100 @@
 'use strict';
 
+
+
+let items = document.getElementById('items');
+let option = document.createElement('option');
+
+
+
 // Cart constructor.
 const Cart = function (items) {
   // this.items is an array of CartItem instances.
-  this.items = items;
+  this.items = new Array;
 };
 
-Cart.prototype.addItem = function (product, quantity) {
+Cart.prototype.addItem = function (product) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
-   let newItem  = new CartItem(product,quantity);
-   this.items.push(newItem);
+  let newItem = new CartItem(product);
+  console.log(newItem);
+  this.items.push(newItem);
 
 };
 
 Cart.prototype.saveToLocalStorage = function () {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
-  localStorage.setItem('cart',JSON.stringify(this.items))
+  let data = JSON.stringify(cart1.items);
+  localStorage.setItem('product', data);
 };
 
 Cart.prototype.removeItem = function (item) {
   // TODO: Fill in this instance method to remove one item from the cart.
   // Note: You will have to decide what kind of parameter to pass in here!
+
+  getter();
+
+for (let i = 0; i < cart1.items.length; i++) {
+  
+  if (cart1.items[i].name === item) {
+
+    console.log('before remove');
+    console.log(cart1.items[i]);
+    const index = cart1.items.indexOf(i);
+
+if (index > -1) {
+  array.splice(index, 1);
+}
+console.log('after remove');
+console.log(cart1.items[i]);
+    break;
+  }  
+}
+//###### End of for loop #####
+
+
+cart1.saveToLocalStorage();
+
+
 };
 
-const CartItem = function (product, quantity) {
+let cart1 = new Cart;
+
+//
+const CartItem = function (product) {
+
   this.product = product;
-  this.quantity = quantity;
+
+
 };
 
 // Product contructor.
 const Product = function (filePath, name) {
   this.filePath = filePath;
   this.name = name;
+  this.quantity = 0;
   Product.allProducts.push(this);
 };
 Product.allProducts = [];
+
+
+
+
+function getter() {
+  
+let storedLocal = localStorage.getItem('product');
+if(storedLocal){
+
+cart1.items = JSON.parse(storedLocal);
+
+//console.log(JSON.parse(storedLocal))
+
+}
+
+}
+
+
+
+
 
 function generateCatalog() {
   new Product('assets/bag.jpg', 'Bag');
@@ -57,7 +117,66 @@ function generateCatalog() {
   new Product('assets/usb.gif', 'USB');
   new Product('assets/water-can.jpg', 'Water Can');
   new Product('assets/wine-glass.jpg', 'Wine Glass');
+
+
+  for (let i = 0; i < Product.allProducts.length; i++) {
+
+    let items = document.getElementById('items');
+    let option = document.createElement('option');
+    items.appendChild(option);
+    option.textContent = Product.allProducts[i].name;
+
+  }
 }
+//############end of generateCatalog#############
+
+let addButton = document.getElementById('catalog');
+addButton.addEventListener('submit', submitter);
+
+
+function submitter(event) {
+
+  event.preventDefault();
+  let labelQuantity = parseInt(event.target.quantity.value);
+  let labelItem = event.target.items.value;
+  // console.log(labelQuantity);
+  // console.log(labelItem);
+
+  for (let i = 0; i < Product.allProducts.length; i++) {
+    if (Product.allProducts[i].name === labelItem) {
+      Product.allProducts[i].quantity += labelQuantity;
+      cart1.addItem(Product.allProducts[i]);
+
+      event.target.quantity.value = 0;
+      event.target.items.value = '';
+
+      if (i === 4) {
+    console.log(Product.allProducts[4].name + 'will get deleted');
+    cart1.removeItem(Product.allProducts[4].name)
+    console.log(Product.allProducts[4].name + 'new index');
+      }
+    
+
+      break;
+    } else if (labelItem === '' || labelQuantity === 0 || labelQuantity === null) {
+      alert('😡 Nothing Got Added To the Cart 😡')
+      break;
+    }
+  }
+
+
+
+  console.log(cart1.items[0]);
+  console.log(cart1.items[1]);
+
+
+}
+
+
+
+  
+  
+
 
 // Initialize the app by creating the big list of products with images and names
 generateCatalog();
